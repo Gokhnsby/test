be6a82ec8b998c14a62456e40408e85e5d068bd4
Gokhnsby <48862948+Gokhnsby@users.noreply.github.com>
2025-06-24 16:48:49 +0200

    Update test_1.ps1

--- test_1.ps1
+++ test_1.ps1
@@ -1,3 +1,4 @@
 Write-Host "Test PowerShell script executed successfully."
 Start-Sleep -Seconds 5
 Write-Host "This script is a safe simulation for remote execution detection use cases."
+Start-Process calc.exe
